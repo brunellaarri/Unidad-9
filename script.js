911100198e0147d419ed16af2713aa84f8294b1d
brunellaarri <barricelador@stbrendans.edu.ar>
2026-09-24 21:00:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let victoriasUsuario = 0;
 let victoriasMaquina = 0;
 
 
-while (victoriasUsuario < 3 && victoriasMaquina < 3) {                 //tiene que ser and asi cuando uno de los dos llega a 3 se termina el juego
+while (victoriasUsuario < 3 && victoriasMaquina < 3) {                 
 
   let jugada = prompt("Elige: Piedra, Papel o Tijera");
-    jugada = jugada.toLowerCase();                                     // lo pongo en lowercase para que no haya problemas con el input del usuario
+    jugada = jugada.toLowerCase();                                     
   let azar = Math.random();
   let jugadaMaquina = "";
 
@@ -18,20 +18,26 @@ while (victoriasUsuario < 3 && victoriasMaquina < 3) {                 //tiene q
   }
 
 
-  console.log(`La máquina eligió: ${jugadaMaquina}`);                      //uso Interpolación de variables
+  console.log(`La máquina eligió: ${jugadaMaquina}`);                      
 
-  if (jugada === "piedra" && jugadaMaquina === "tijera") {                 // uso los acumuladores para sumar las victorias y que en algun momento termine el while
+  if (jugada === "piedra" && jugadaMaquina === "tijera") {                
     victoriasUsuario++;
+    alert(`¡Ganaste la ronda! La máquina eligió ${jugadaMaquina}.\nMarcador: Tú ${victoriasUsuario} - ${victoriasMaquina} Máquina`);
   } else if (jugada === "papel" && jugadaMaquina === "piedra") {
     victoriasUsuario++;
+    alert(`¡Ganaste la ronda! La máquina eligió ${jugadaMaquina}.\nMarcador: Tú ${victoriasUsuario} - ${victoriasMaquina} Máquina`);
   } else if (jugada === "tijera" && jugadaMaquina === "papel") {
     victoriasUsuario++;
+    alert(`¡Ganaste la ronda! La máquina eligió ${jugadaMaquina}.\nMarcador: Tú ${victoriasUsuario} - ${victoriasMaquina} Máquina`);
   } else if (jugada === jugadaMaquina) {
     console.log("Empate");
+    alert(`Empate. La máquina eligió ${jugadaMaquina}.\nMarcador: Tú ${victoriasUsuario} - ${victoriasMaquina} Máquina`);
   } else {
     victoriasMaquina++;
+    alert(`Gana la máquina. Eligió ${jugadaMaquina}.\nMarcador: Tú ${victoriasUsuario} - ${victoriasMaquina} Máquina`);
   }
 }
 
-console.log(victoriasUsuario === 3 ? "Ganó el usuario" : "Ganó la máquina");        //uso operador ternario en vez de if o else 
-
+let mensajeFinal = victoriasUsuario === 3 ? "Ganó el usuario el juego" : "Ganó la máquina el juego";
+console.log(mensajeFinal);
+alert(mensajeFinal);
